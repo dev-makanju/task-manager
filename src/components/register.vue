7b79ff156d7d430757420e5e40c46ae37cd68297
oluwafemi <makurseme@gmail.com>
2022-01-18 17:32:46 +0100

    added a sign up page or ui

---
+++ src/components/register.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="form-control">
+        <div class="form-control header">
+             <h1>Create Account</h1>
+        </div>
+        <div class="form-control-inputs">
+            <label for="email">Email:</label>
+            <input type="email" name="" id="email" placeholder="Enter your email">
+        </div>
+        <div class="form-control-inputs">
+            <label for="email">Username:</label>
+            <input type="text" name="" id="" placeholder="Enter your username">
+        </div>
+        <div class="form-control-inputs">
+            <label for="password">Password:</label>
+            <input type="password" name="" id="password" placeholder="***************">
+        </div>
+        <div class="form-control-inputs">
+            <label for="password">Confirm Password:</label>
+            <input type="password" name="" id="password" placeholder="***************">
+        </div>
+        <div class="form-control-inputs">
+              <button class="btn btn-auth">Sign Up</button>
+        </div>
+        <p>Already a menber?
+            <router-link :to="{name:'signIn'}">login</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"register"    
+}
+</script>
--- src/views/signUp.vue
+++ src/views/signUp.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+       <register/>
   </div>
 </template>
+<script>
+import register from '../components/register.vue'
+
+export default {
+   name:"registerPage",
+   components:{
+     register
+   }
+}
+</script>
