112f40e1827cf59dc5c95d4b0bfe80fbbd0ce1e3
oluwafemi <makurseme@gmail.com>
2022-01-18 01:46:13 +0100

    completed css deefaults

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,16 @@ export default {
 </script>
 
 <style>
+/**********
+*Add fonts
+**********/
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital@1&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital@1&family=Montserrat:ital@1&display=swap');
+
+
+
+
 *{
   margin: 0;
   padding: 0;
@@ -34,7 +44,7 @@ p{
    border-radius: 0.3rem;
    cursor: pointer;
    margin-top: 1rem;
-
+   font-family: 'Montserrat', sans-serif;
 }
 
 h1 , h2 , h3 , h4 , h5 , h6{
@@ -46,13 +56,13 @@ h1 , h2 , h3 , h4 , h5 , h6{
   border-radius: 0.3rem;
   cursor: pointer;
   margin-top: 1rem;
-
-
+  font-family: 'Montserrat Alternates', sans-serif;
 }
 
 ul , ol , li , a {
   list-style-type: none ;
   text-decoration: none ;
+  font-family: 'Montserrat', sans-serif;
 }
 
 html {
@@ -82,7 +92,31 @@ body{
   color: #fff;
 }
 
+::-webkit-scrollbar {
+    width: 10px;
+    background:lightgray;
+}
 
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgb(189, 198, 201); 
+    border-radius: 17px;
+}
 
+::-webkit-scrollbar-thumb {
+    border-radius: 17px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
+    background:#e3e8e9;
+} 
+
+.btn{
+   width: 100%;
+   padding: 5px 10px;
+   box-shadow: 0px 20xp 5px rgba(0 , 0 , 0 , .3);
+   -webkit-box-shadow: 0px 20xp 5px rgba(0 , 0 , 0 , .3);
+   border-radius: 5px;
+   -webkit-border-radius: 5px;
+   -moz-border-radius: 5px;
+   -ms-border-radius: 5px;
+}
 
 </style>
